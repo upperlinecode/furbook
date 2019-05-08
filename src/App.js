6aab535dd74f083bd526afc11110a903904a1ed2
jolson615 <jolson615@gmail.com>
2019-05-08 14:40:21 -0400

    updates with closures

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import Descriptions from './components/descriptions.js';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  const component = new React.Component()
+
+  component.render = () => {
+    return(
       <div className="App">
         <Navbar/>
-		<Splash/>
-		<Photos/>
-		<Descriptions/>
+        <Splash/>
+      	<Photos/>
+      	<Descriptions/>
       </div>
-    );
+    )
   }
+
+  return component
 }
 
 export default App;
