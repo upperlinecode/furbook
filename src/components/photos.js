dde210a11764529a91cd3d8920d64bb4d804e861
jolson615 <jolson615@gmail.com>
2020-08-13 11:58:47 -0400

    spaces and semicolons

--- src/components/photos.js
+++ src/components/photos.js
@@ -6,21 +6,21 @@ import './css/photos.css';
 
 
 function Photos() {
-	return(
-		<div className='container-fluid Photos'>
-			<div className='row'>
-				<div className='col'>
-					<img src={ cat } alt="cat"/>
-				</div>
-				<div className='col'>
-					<img src={ dog2 } alt="small dog"/>
-				</div>
-				<div className='col'>
-					<img src={ dog1 } alt="large dog"/>
-				</div>
-			</div>
-		</div>
-	);
+  return(
+    <div className='container-fluid Photos'>
+      <div className='row'>
+        <div className='col'>
+          <img src={ cat } alt="cat"/>
+        </div>
+        <div className='col'>
+          <img src={ dog2 } alt="small dog"/>
+        </div>
+        <div className='col'>
+          <img src={ dog1 } alt="large dog"/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Photos;
